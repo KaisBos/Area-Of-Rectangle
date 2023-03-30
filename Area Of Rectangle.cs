5ee5a1649a8e5af99ae1c9cd154c0d3f911a84db
KaisBos <94121595+KaisBos@users.noreply.github.com>
2023-03-30 23:53:49 +0700

    Area Of Rectangle

---
+++ Area Of Rectangle.cs
@@ -0,0 +1,9 @@
+﻿float width;
+float height;
+
+Console.WriteLine("Enter Width: ");
+width = float.Parse(Console.ReadLine());
+Console.WriteLine("Enter Height: ");
+height = float.Parse(Console.ReadLine());
+
+Console.WriteLine("The Area Of Rectangle: " + (width*height));
